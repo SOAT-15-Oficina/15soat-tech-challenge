8e4f8d5be7063d5ed11f717b94dd9eb9b9c10e86
Emerson Santana <emerson.santana@acordocerto.com.br>
2026-04-27 20:36:24 -0300

    :recycle: refactor: reorganizar estrutura de banco de dados e migrações

--- db/migrations/001_create_supplies_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS supplies (
-    id         UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
-    service_id UUID    NOT NULL,
-    item_id    UUID    NOT NULL,
-    quantity   INTEGER NOT NULL
-);
